b566b52f8c797791f6742c17afb0c9059ecbc9bf
ganesha <ganesha@ganesha-HP-Laptop-15q-ds0xxx>
2020-07-15 11:37:08 +0800

    Project Structure Organizer

--- demoSpring3/src/main/java/com/sap/cloud/customSpringApps/hrm/code/controller/helloWorld.java
+++ demoSpring3/src/main/java/com/sap/cloud/customSpringApps/hrm/code/controller/helloWorld.java
@@ -1,4 +1,4 @@
-package com.sap.cloud.customSpringApps.hrm.code;
+package com.sap.cloud.customSpringApps.hrm.code.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
